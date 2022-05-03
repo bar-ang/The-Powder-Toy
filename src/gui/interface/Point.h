02bfed9d9bc9ec0b090cf31a8b8aa756f2bec0f5
Bar <bar@lightbitslabs.com>
2022-05-03 11:46:20 +0300

    ui::Point for 3D

--- src/gui/interface/Point.h
+++ src/gui/interface/Point.h
@@ -14,131 +14,142 @@ struct Point
 
 	POINT_T X;
 	POINT_T Y;
+	POINT_T Z;
 
-	Point(POINT_T x, POINT_T y)
+	Point(POINT_T x, POINT_T y, POINT_T z =  static_cast<POINT_T>(0))
 	: X(x)
 	, Y(y)
+	, Z(z)
 	{
 	}
 
 	inline Point operator - () const
 	{
-		return Point(-X, -Y);
+		return Point(-X, -Y, -Z);
 	}
 
 	inline Point operator + (const Point& v) const
 	{
-		return Point(X + v.X, Y + v.Y);
+		return Point(X + v.X, Y + v.Y, Z + v.Z);
 	}
 
 	inline Point operator + (const int v) const
 	{
-		return Point(X + v, Y + v);
+		return Point(X + v, Y + v, Z + v);
 	}
 
 	inline Point operator - (const Point& v) const
 	{
-		return Point(X - v.X, Y - v.Y);
+		return Point(X - v.X, Y - v.Y, Z - v.Z);
 	}
 
 	inline Point operator - (const int v) const
 	{
-		return Point(X - v, Y - v);
+		return Point(X - v, Y - v, Z - v);
 	}
 
 	inline Point operator * (const Point& v) const
 	{
-		return Point(X * v.X, Y * v.Y);
+		return Point(X * v.X, Y * v.Y, Z * v.Z);
 	}
 
 	inline Point operator * (int v) const
 	{
-		return Point(X * static_cast<POINT_T>(v), Y * static_cast<POINT_T>(v));
+		return Point(X * static_cast<POINT_T>(v), Y * static_cast<POINT_T>(v), Z * static_cast<POINT_T>(v));
 	}
 
 	inline Point operator * (float v) const
 	{
-		return Point(X * static_cast<POINT_T>(v), Y * static_cast<POINT_T>(v));
+		return Point(X * static_cast<POINT_T>(v), Y * static_cast<POINT_T>(v), Z * static_cast<POINT_T>(v));
 	}
 
 	inline Point operator / (const Point& v) const
 	{
-		return Point(X / v.X, Y / v.Y);
+		return Point(X / v.X, Y / v.Y, Z / v.Z);
 	}
 
 	inline Point operator / (int v) const
 	{
-		return Point(X / static_cast<POINT_T>(v), Y / static_cast<POINT_T>(v));
+		return Point(X / static_cast<POINT_T>(v), Y / static_cast<POINT_T>(v), Z / static_cast<POINT_T>(v));
 	}
 
 	inline Point operator / (float v) const
 	{
-		return Point(X / static_cast<POINT_T>(v), Y / static_cast<POINT_T>(v));
+		return Point(X / static_cast<POINT_T>(v), Y / static_cast<POINT_T>(v), Z / static_cast<POINT_T>(v));
 	}
 
 	inline void operator += (const Point& v)
 	{
 		X += v.X;
 		Y += v.Y;
+		Z += v.Z;
 	}
 
 	inline void operator -= (const Point& v)
 	{
 		X -= v.X;
 		Y -= v.Y;
+		Z -= v.Z;
 	}
 
 	inline void operator *= (const Point& v)
 	{
 		X *= v.X;
 		Y *= v.Y;
+		Z *= v.Z;
 	}
 
 	inline void operator *= (int v)
 	{
 		X *= static_cast<POINT_T>(v);
 		Y *= static_cast<POINT_T>(v);
+		Z *= static_cast<POINT_T>(v);
 	}
 
 	inline void operator *= (float v)
 	{
 		X *= static_cast<POINT_T>(v);
 		Y *= static_cast<POINT_T>(v);
+		Z *= static_cast<POINT_T>(v);
 	}
 
 	inline void operator /= (const Point& v)
 	{
 		X /= v.X;
 		Y /= v.Y;
+		Z /= v.Z;
 	}
 
 	inline void operator /= (int v)
 	{
 		X /= static_cast<POINT_T>(v);
 		Y /= static_cast<POINT_T>(v);
+		Z /= static_cast<POINT_T>(v);
 	}
 
 	inline void operator /= (float v)
 	{
 		X /= static_cast<POINT_T>(v);
 		Y /= static_cast<POINT_T>(v);
+		Z /= static_cast<POINT_T>(v);
 	}
 
 	inline bool operator == (const Point& v) const
 	{
-		return (X == v.X && Y == v.Y);
+		return (X == v.X && Y == v.Y && Z == v.Z);
 	}
 
 	inline bool operator != (const Point& v) const
 	{
-		return (X != v.X || Y != v.Y);
+		return (X != v.X || Y != v.Y || Z != v.Z);
 	}
 
 	inline Point operator = (const Point& v)
 	{
 		X = v.X;
 		Y = v.Y;
-		return Point(X, Y);
+		Z = v.Z;
+		return Point(X, Y, Z);
 	}
 
 };
